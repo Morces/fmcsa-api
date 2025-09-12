f53d75a9e42ae9dd6b55b8eb102cdeb1ee1abd76
Morces <karanim594@gmail.com>
2025-09-12 16:07:21 +0300

    updates requirements.txt

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 load_dotenv()
+from decouple import config
+import dj_database_url 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -98,10 +100,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        config("DATABASE_URL"),
+        conn_max_age=600,   # keeps connection alive
+        ssl_require=True,   # important for Supabase
+    )
 }
 
 
